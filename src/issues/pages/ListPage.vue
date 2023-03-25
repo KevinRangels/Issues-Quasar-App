9906aeeb76e197256af9ed18e2c408aa726cc2de
Kevin Rangel <krangel107@gmail.com>
2023-03-24 22:00:41 -0500

    Create base structure

---
+++ src/issues/pages/ListPage.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import LoaderSpinner from 'src/shared/components/LoaderSpinner.vue';
+import FilterSelector from '../components/filter-selector/FilterSelector.vue';
+
+
+</script>
+
+<template>
+    <div class="row q-mb-md">
+        <div class="col-12">
+            <span class="text-h4">GitHub Issues</span>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-md-4">
+            <!-- TODO: Filtros -->
+            <FilterSelector/>
+        </div>
+        <div class="col-xs-12 col-md-4">
+            <!-- TODO: Loader -->
+            <LoaderSpinner />
+            <!-- TODO: IssueList -->
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ src/shared/components/LoaderSpinner.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+    interface Props {
+        color?: string;
+        size?: string;
+        thickness?: number;
+        showText?: boolean
+    }
+
+    const props = withDefaults(defineProps<Props>(), {
+        color: 'primary',
+        size: '3.5rem',
+        thickness: 5,
+        showText: true
+    });
+
+</script>
+
+<template>
+    <div class="column items-center">
+        <q-spinner :color="props.color" :size="props.size" :thickness="props.thickness" />
+        <span v-if="props.showText" class="q-mt-sm">Loading</span>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
